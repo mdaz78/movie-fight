28b3faf14e9613c2e44b1195a82571218414d233
Md Abu Zafar <mdabuzafar78@gmail.com>
2020-09-22 01:34:30 +0530

    feat(movie-detail): Add movie details

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-const URL = "http://www.omdbapi.com/?apikey=5d21fc80";
-const fetchData = async (searchTerm) => {
-  const response = await fetch(`${URL}&s=${searchTerm}`);
-  const data = await response.json();
-
-  if (data.Error) {
-    return [];
-  }
-
-  const { Search: searchResults } = data;
-  return searchResults;
-};
-
 const rootOfAutocomplete = document.querySelector(".autocomplete");
 rootOfAutocomplete.innerHTML = `
 <label class="bold">Search for a movie</label>
@@ -38,7 +25,8 @@ const onInput = async (event) => {
   resultsWrapper.innerHTML = "";
   dropdown.classList.add("is-active");
 
-  movies.forEach(({ Poster: poster, Title: title }) => {
+  movies.forEach((movie) => {
+    const { Poster: poster, Title: title } = movie;
     const movieOption = document.createElement("a");
     const imgSrc = poster === "N/A" ? "" : poster;
     movieOption.classList.add("dropdown-item");
@@ -50,10 +38,67 @@ const onInput = async (event) => {
     movieOption.addEventListener("click", () => {
       dropdown.classList.add("is-active");
       input.value = title;
+      onMovieSelect(movie);
     });
 
     resultsWrapper.append(movieOption);
   });
+};
+
+const onMovieSelect = async (movie) => {
+  const movieData = await fetchData(movie.imdbID, "i");
+  document.querySelector("#summary").innerHTML = movieTemplate(movieData);
+};
+
+const movieTemplate = (movieDetail) => {
+  const {
+    Poster: poster,
+    Title: title,
+    Genre: genre,
+    Plot: plot,
+    Awards: awards,
+    BoxOffice: boxOffice,
+    Metascore: metaScore,
+    imdbRating,
+    imdbVotes,
+  } = movieDetail;
+
+  return `
+  <article class="media">
+    <figure class="media-left">
+      <p class="image">
+        <img src="${poster}" alt="Poster of Movie">
+      </p>
+    </figure>
+    <div class="media-content">
+      <div class="content">
+        <h1>${title}</h1>
+        <h4>${genre}</h4>
+        <p>${plot}</p>
+      </div>
+    </div>
+  </article>
+  <article class="notification is-primary">
+    <p class="title">${awards}</p>
+    <p class="sub-title">Awards</p>
+  </article>
+  <article class="notification is-primary">
+    <p class="title">${boxOffice}</p>
+    <p class="sub-title">Box Office</p>
+  </article>
+  <article class="notification is-primary">
+    <p class="title">${metaScore}</p>
+    <p class="sub-title">Meta Score</p>
+  </article>
+  <article class="notification is-primary">
+    <p class="title">${imdbRating}</p>
+    <p class="sub-title">IMDB Rating</p>
+  </article>
+  <article class="notification is-primary">
+    <p class="title">${imdbVotes}</p>
+    <p class="sub-title">IMDB Votes</p>
+  </article>
+  `;
 };
 
 input.addEventListener("input", debounce(onInput, 500));
--- utils.js
+++ utils.js
@@ -1,9 +1,11 @@
-/** 
+const URL = "http://www.omdbapi.com/?apikey=5d21fc80";
+
+/**
  * debounce waits for a certain time before executing the given function
  * @param {function} fn - function to execute
  * @param {number} delay - delay in ms to execute received function
  * @returns {function}
-*/
+ */
 const debounce = (fn, delay = 1000) => {
   let timeoutId;
   return (...args) => {
@@ -12,4 +14,26 @@ const debounce = (fn, delay = 1000) => {
       fn.apply(null, args);
     }, delay);
   };
+};
+
+/**
+ * returns a promise that resolves into either an empty array
+ * or movieSearchResults or just the requested data in json format
+ * @param {String} searchTerm - term to search
+ * @param {String} paramName - parameter to be passed defaults to "s"
+ */
+const fetchData = async (searchTerm, paramName = "s") => {
+  const response = await fetch(`${URL}&${paramName}=${searchTerm}`);
+  const data = await response.json();
+
+  if (data.Error) {
+    return [];
+  }
+
+  if (data.Search) {
+    const { Search: searchResults } = data;
+    return searchResults;
+  }
+
+  return data;
 };
